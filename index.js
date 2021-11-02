64fd8f2959608bdeaaf796f1914fa77583ab5a34
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-11-02 14:45:28 +0000

    Code Challenge 11

--- index.js
+++ index.js
@@ -1,27 +1,63 @@
 //1
-function areBothSame(){
-  
+// Create two paramaters
+function areBothSame(num1, num2) {
+    // if paramater1 is equal in value to paramater2, return true
+    // if (num1 == num2) {
+    //     return true;
+    //     // else return false
+    // } else {
+    //     return false;
+    // }
+    return num1 == num2;
 }
+
+// console.log(areBothSame(1, '1'));
 
 //2
-function areBothEqual(){
-  
+// Create two paramaters
+function areBothEqual(num1, num2) {
+    // if paramater1 is equal in value & data type to paramater2, return true
+    // else return false
+    return num1 === num2;
 }
+
+// console.log(areBothEqual(true, 1));
 
 //3
-function areAllFourEqual(){
-  
+// Create four paramaters
+function areAllFourEqual(num1, num2, num3, num4) {
+    // if all four paramaters are equal in value & data type, return true
+    // else return false
+    return (num1 === num2) && (num2 === num3) && (num3 === num4);
 }
 
+// console.log(areAllFourEqual("2", "2", "2", 2));
 
 //4
-function areAllFourSame(){
-  
+// Create four paramaters
+function areAllFourSame(num1, num2, num3, num4) {
+    // if all four paramaters are equal in value, return true
+    // else return false 
+    return (num1 == num2) && (num2 == num3) && (num3 == num4);
 }
+
+// console.log(areAllFourSame(true, true, 2, "1"));
 
 //5
-function areAllFourSameOrEqual(){
-  
+// Create four paramaters
+function areAllFourSameOrEqual(num1, num2, num3, num4) {
+    // if all four paramaters are equal in both value & data type, return the string 'equal'
+    // else if all four paramters are equal in value, but not data type, return the string 'same'
+    // else return null
+    let e = 'equal';
+    let s = 'same';
+    if ((num1 === num2) && (num2 === num3) && (num3 === num4)) {
+        return e;
+    } else if ((num1 == num2) && (num2 == num3) && (num3 == num4)) {
+        return s;
+    } else {
+        return null;
+    }
 }
 
-
+console.log(areAllFourSameOrEqual("2", 2, 1, "2"));
